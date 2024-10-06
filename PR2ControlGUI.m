ddfa074d63d57cfcb2278a031b3f0adbb00eac5b
jackson <jackrussell772@gmail.com>
2024-10-06 17:58:55 +1100

    matlab app sucks, made raw UI better

--- PR2ControlGUI.m
+++ PR2ControlGUI.m
@@ -5,86 +5,85 @@ grid on;
 hold on;
 axis([-4.5 4.5 -3.5 3.5 0 3.5]);
 
-view(90, 10); 
+view(90, 10);
+
+% Number of joints for the right and left arms
+numJointsR = 7;
+numJointsL = 7;
+
+% Create a structure to hold the PR2 robot objects and sliders
+obj.pr2Left = PR2Left();
+obj.pr2Right = PR2Right();
+
+% Retrieve the joint limits (qlims) for both arms
+qlimR = obj.pr2Right.model.qlim;  % Right arm joint limits
+qlimL = obj.pr2Left.model.qlim;   % Left arm joint limits
+
+%Position = [left, bottom, width, height]
+sliderPanel = uipanel('Title', 'Joint Control', 'FontSize', 12, ...
+    'Position', [0 0.05 0.9 0.20]); % 90% of figure width, 20% height, 0% from the left side, 0.05% from the bottom 
+for i = 1:numJointsL
+    % Joint label
+    uicontrol('Style', 'text', 'Parent', sliderPanel, ...
+        'Position', [50, 25 + (i-1)*30, 100, 30], ...
+        'String', ['Left Joint ' num2str(i)], ...
+        'FontSize', 12, 'FontWeight', 'bold', ...
+        'ForegroundColor', [0 0 1]); % Blue text
+    defaultValueL = (qlimL(i, 1) + qlimL(i, 2)) / 2;
+    % Create slider for left arm joint with custom colors
+    obj.leftSliders(i) = uicontrol('Style', 'slider', ...
+        'Parent', sliderPanel, ...
+        'Min', qlimL(i, 1), 'Max', qlimL(i, 2), ...
+        'Value', defaultValueL, ...
+        'Position', [150, 25 + (i-1)*30, 300, 30], ...
+        'Callback', @(src, event) updateRobotLeft(i), ...
+        'BackgroundColor', [0.8 0.8 0.8], ... % Set background color
+        'ForegroundColor', [0.2 0.6 0.2], ... % Set slider color
+        'SliderStep', [0.01 0.1]); % Set step size
     
-    % Number of joints for the right and left arms
-    numJointsR = 7;
-    numJointsL = 7;
+    % Display current slider value
+    obj.leftSliderValueText(i) = uicontrol('Style', 'text', ...
+        'Parent', sliderPanel, ...
+        'Position', [460, 25 + (i-1)*30, 100, 30], ...
+        'String', num2str(defaultValueL, '%.2f'), ...
+        'FontSize', 12, 'FontWeight', 'bold', ...
+        'ForegroundColor', [0 0 0]); % Black text
+end
 
-    % Create a structure to hold the PR2 robot objects and sliders
-    obj.pr2Left = PR2Left();
-    obj.pr2Right = PR2Right();
-
-    % Retrieve the joint limits (qlims) for both arms
-    qlimR = obj.pr2Right.model.qlim;  % Right arm joint limits
-    qlimL = obj.pr2Left.model.qlim;   % Left arm joint limits
-
-    % Create a panel for the robot visualization
-    %modelPanel = uipanel('Title', 'PR2 Model', 'FontSize', 12, ...
-    %    'Position', [0.05 0.2 0.6 0.75]); % 60% of figure width, 75% height
-    %
-    % Create an axes inside the modelPanel to plot the robot
-    %modelAxes = axes('Parent', modelPanel);
-    %axis(modelAxes, 'equal');
-    %hold(modelAxes, 'on');
+sliderPanelR = uipanel('Title', 'Joint Control', 'FontSize', 12, ...
+        'Position', [0.35, 0.05, 0.5, 0.20]); t   
+for i = 1:numJointsR
     
-    % Set the current axes for plotting
-    %axes(modelAxes);
+    uicontrol('Style', 'text', 'Parent', sliderPanelR, ...
+        'Position', [50, 25 + (i-1)*30, 100, 30], ...
+        'String', ['Right Joint ' num2str(i)], ...
+        'FontSize', 12, 'FontWeight', 'bold', ...
+        'ForegroundColor', [0 0 1]); 
+    defaultValueR = (qlimR(i, 1) + qlimR(i, 2)) / 2;
 
-    % Plot the initial PR2 models in the axes
-    %obj.pr2Right.model.plot(zeros(1, numJointsR));
-    %obj.pr2Left.model.plot(zeros(1, numJointsL));
+    obj.rightSliders(i) = uicontrol('Style', 'slider', ...
+        'Parent', sliderPanelR, ...
+        'Min', qlimR(i, 1), 'Max', qlimR(i, 2), ...
+        'Value', defaultValueR, ...
+        'Position', [150, 25 + (i-1)*30, 300, 30], ...
+        'Callback', @(src, event) updateRobotRight(i), ...
+        'BackgroundColor', [0.8 0.8 0.8], ... 
+        'ForegroundColor', [0.2 0.6 0.2], ...
+        'SliderStep', [0.01 0.1]); 
+    
+    obj.rightSliderValueText(i) = uicontrol('Style', 'text', ...
+        'Parent', sliderPanelR, ...
+        'Position', [460, 25 + (i-1)*30, 100, 30], ...
+        'String', num2str(defaultValueR, '%.2f'), ...
+        'FontSize', 12, 'FontWeight', 'bold', ...
+        'ForegroundColor', [0 0 0]); 
+end
 
-    % Sliders and labels for left arm joints
-    for i = 1:numJointsL
-        % Joint label
-        uicontrol('Style', 'text', 'Position', [50, 400 - (i-1)*50, 100, 30], ...
-                  'String', ['Left Joint ' num2str(i)]);
-        
-        % Slider default value as midpoint ofjoint's qlim
-        defaultValueL = (qlimL(i, 1) + qlimL(i, 2)) / 2;
-        
-        % Create slider for left arm joint
-        obj.leftSliders(i) = uicontrol('Style', 'slider', ...
-            'Min', qlimL(i,1), 'Max', qlimL(i,2), 'Value', defaultValueL, ...
-            'Position', [150, 400 - (i-1)*50, 300, 30], ...
-            'Callback', @(src, event) updateRobotLeft(i));
-        
-        % Display current slider value
-        obj.leftSliderValueText(i) = uicontrol('Style', 'text', ...
-            'Position', [460, 400 - (i-1)*50, 100, 30], ...
-            'String', num2str(defaultValueL, '%.2f'));
-    end
-
-    % Create animate button for the left arm
-    uicontrol('Style', 'pushbutton', 'String', 'Animate Left', ...
-              'Position', [250, 50, 80, 40], 'Callback', @(src, event) animateRobotLeft());
-
-    for i = 1:numJointsR
-
-        uicontrol('Style', 'text', 'Position', [550, 400 - (i-1)*50, 100, 30], ...
-                  'String', ['Right Joint ' num2str(i)]);
-        
-        defaultValueR = (qlimR(i, 1) + qlimR(i, 2)) / 2;
-        
-        obj.rightSliders(i) = uicontrol('Style', 'slider', ...
-            'Min', qlimR(i,1), 'Max', qlimR(i,2), 'Value', defaultValueR, ...
-            'Position', [650, 400 - (i-1)*50, 300, 30], ...
-            'Callback', @(src, event) updateRobotRight(i));
-        
-        obj.rightSliderValueText(i) = uicontrol('Style', 'text', ...
-            'Position', [960, 400 - (i-1)*50, 100, 30], ...
-            'String', num2str(defaultValueR, '%.2f'));
-    end
-
-    uicontrol('Style', 'pushbutton', 'String', 'Animate Right', ...
-              'Position', [500, 50, 80, 40], 'Callback', @(src, event) animateRobotRight());
-
-    % Function to update the right arm position
+% Function to update the right arm position
     function updateRobotRight(index)
-        qRight = zeros(1, numJointsR);  
+        qRight = zeros(1, numJointsR);
         for j = 1:numJointsR
-            qRight(j) = obj.rightSliders(j).Value;  
+            qRight(j) = obj.rightSliders(j).Value;
         end
         
         % Update the slider value display
@@ -93,11 +92,11 @@ view(90, 10);
         obj.pr2Right.model.animate(qRight);
     end
 
-    % Function to update the left arm position
+% Function to update the left arm position
     function updateRobotLeft(index)
-        qLeft = zeros(1, numJointsL);  
+        qLeft = zeros(1, numJointsL);
         for j = 1:numJointsL
-            qLeft(j) = obj.leftSliders(j).Value;  
+            qLeft(j) = obj.leftSliders(j).Value;
         end
         
         set(obj.leftSliderValueText(index), 'String', num2str(qLeft(index), '%.2f'));
@@ -105,7 +104,7 @@ view(90, 10);
         obj.pr2Left.model.animate(qLeft);
     end
 
-    % Function to animate the left arm
+% Function to animate the left arm
     function animateRobotLeft()
         qLeft = zeros(1, numJointsL);
         for j = 1:numJointsL
@@ -116,7 +115,7 @@ view(90, 10);
         obj.pr2Left.model.animate(qLeft);
     end
 
-    % Function to animate the right arm
+% Function to animate the right arm
     function animateRobotRight()
         qRight = zeros(1, numJointsR);
         for j = 1:numJointsR
@@ -127,6 +126,9 @@ view(90, 10);
         obj.pr2Right.model.animate(qRight);
     end
 end
+
+
+
 
 
 
--- pr2_gui.m
+++ pr2_gui.m
@@ -5,14 +5,13 @@ axis equal;
 
 pr2Left = PR2Left(); 
 pr2Right = PR2Right(); 
-
 GUIAPP = pr2gui();
 
 % Load the PR2 robots into the GUI
 GUIAPP.getRobot(pr2Left, pr2Right); 
 
 % Main loop for animation
-while true
+while(1)
     
     [qLeft, qRight] = GUIAPP.updateQvalues(); % Method to get joint values
     % Animate the PR2 models
