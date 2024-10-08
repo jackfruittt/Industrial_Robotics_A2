1ee4a611201c416ded3f0854ec3a98afb53bc50c
Jackson <jackrussell772@gmail.com>
2024-10-09 02:43:06 +1100

    Tidy Up

--- EnvironmentLoader.m
+++ EnvironmentLoader.m
@@ -21,7 +21,7 @@ classdef EnvironmentLoader
         function obj = EnvironmentLoader() % Constructor to initialize the environment
             %
             obj.loadCustomObjects();
-            compEnv = 0;
+            compEnv = 1;
             if compEnv
                 obj.pr2Left = PR2Left();
                 obj.pr2Right = PR2Right();
@@ -29,7 +29,6 @@ classdef EnvironmentLoader
                 obj.gripperr1 = PR2RightGripper();
                 obj.gripperl2 = PR2LeftGripper();
                 obj.gripperr2 = PR2RightGripper();
-                obj.gripperrk = PR2RightGripperWithKnife();
                 light('Position', [1 1 1], 'Style', 'infinite');
                 lighting gouraud;  
                 material shiny;   
