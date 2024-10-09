1b80bd8e4746d325abbf216fdc52516d6c31fc7d
Jackson <jackrussell772@gmail.com>
2024-10-09 18:51:45 +1100

    Made a new superior control class to replace the pr2Control. Will have code to control all robots. Tested and working. More control functions tba

--- main.m
+++ main.m
@@ -10,7 +10,8 @@ view(90, 10);
 eStop = serial('COM3', 'BaudRate', 9600);  
 %fopen(eStop);
 env = EnvironmentLoader();
-pr2 = pr2Control(env);
+%pr2 = pr2Control(env);
+robot = robotControl(env);
 laser = pr2Laser();
 gripperLeftState = 'closed';
 gripperRightState = 'closed';
@@ -36,6 +37,10 @@ Tb3l = transl(0, 0.490, 1.083);
 Tb4r = transl(0.116, -0.669, 0.334);
 Tb4l = transl(0.99, 0.294, 0.286);
 
+startPos = transl(2, 0.764, 1.092);
+
+endPos = transl(1.885, -0.555, 0.207);
+
 %% TO USE PR2 ANIMATION FUNCTIONS
 % pr2.animateRightPR2ArmsAndGrippers(RightStartPos, RightEndPos, numSteps);
 % pr2.animateLeftPR2ArmsAndGrippers(LeftStartPos, LeftEndPos, numSteps);
@@ -47,6 +52,8 @@ Tb4l = transl(0.99, 0.294, 0.286);
 % pr2.RightGripperOpen(numSteps);
 
 %pr2.bothGripperClose(50);
+
+%{
 pr2.animatePR2ArmsAndGrippers(homePosr, Tb2r, homePosl, Tb3l, numSteps, eStop);
 
 % Scan for the banana
@@ -86,3 +93,48 @@ qRightEnd = pr2.env.pr2Right.model.ikcon(rightEnd);
 qWpMat = [qRightStart; rightWpTwo; rightWpTwo; qRightEnd];
 
 pr2.animateRightPR2ArmsAndGrippersWithWaypoints(qWpMat, numSteps, eStop);
+%}
+
+%{
+robot.animatePR2ArmsAndGrippers(homePosr, Tb2r, homePosl, Tb3l, numSteps, eStop);
+
+% Scan for the banana
+[firstCoord, lastCoord] = laser.laser_scan(sensor_position, laser_rotation, radii, centerPoint);
+
+fprintf('First Coordinate: (%.2f, %.2f, %.2f)\n', firstCoord(1), firstCoord(2), firstCoord(3));
+fprintf('Last Coordinate: (%.2f, %.2f, %.2f)\n', lastCoord(1), lastCoord(2), lastCoord(3));
+
+% Create T-matrices for the banana start and end
+bananaR = transl(firstCoord);
+bananaL = transl(lastCoord);
+
+robot.PR2LeftGripperOpen(50, eStop);
+robot.PR2LeftGripperClose(50, eStop);
+robot.PR2RightGripperOpen(50, eStop);
+robot.PR2RightGripperClose(50, eStop);
+robot.PR2BothGrippersOpen(50, eStop);
+robot.PR2BothGrippersClose(50, eStop);
+robot.PR2LeftGripperOpen(50, eStop);
+robot.animatePR2ArmsAndGrippers(Tb3r, Tbr, Tb3l, Tbl, numSteps, eStop);
+robot.animatePR2ArmsAndGrippers(Tbr, Tb2r, Tbl, Tb2l, numSteps, eStop);
+robot.animate(Tb2r, Tb3r, numSteps, eStop);
+robot.PR2RightGripperOpen(50, eStop);
+robot.animateLeftPR2ArmsAndGrippers(Tb2l, Tb3l, numSteps, eStop);
+
+% Move to banana
+robot.animatePR2ArmsAndGrippers(Tb3r, bananaR, Tb3l, bananaL, numSteps, eStop);
+robot.PR2BothGrippersClose(50, eStop);
+robot.animatePR2RightArmsAndGrippers(bananaR, homePosr, numSteps, eStop);
+rightStart = transl(0.821, -0.440, 1);
+qRightStart = pr2.env.pr2Right.model.ikcon(rightStart);
+rightWpOne = deg2rad([89.4 90 0 0 0 0 0]);
+rightWpTwo = deg2rad([90 67.2 0 15 0 0 0]);
+rightEnd = transl(0.172, -0.793, 0.269);
+qRightEnd = pr2.env.pr2Right.model.ikcon(rightEnd);
+
+qWpMat = [qRightStart; rightWpTwo; rightWpTwo; qRightEnd];
+
+pr2.animateRightPR2ArmsAndGrippersWithWaypoints(qWpMat, numSteps, eStop);
+%}
+robot.animateTM5(startPos, endPos, numSteps, eStop);
+
